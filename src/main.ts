7c379baf763a55e11137f7bee090b3421cf4f64e
EGGSY <eggsydev@outlook.com>
2021-05-30 02:28:16 +0300

    refactor: use router

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,19 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
+// Import Vue router, routes and create the router
+import { createRouter, createWebHistory } from "vue-router";
+import routes from "./routes";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
 // Import Windi CSS
 import "virtual:windi.css";
 
-createApp(App).mount("#app");
+// Create and mount the app
+createApp(App)
+  .use(router)
+  .mount("#app");
