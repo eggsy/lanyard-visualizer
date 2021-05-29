8e7d33567ae142b2579f3e2466aa389d50e5a7dc
EGGSY <eggsydev@outlook.com>
2021-05-30 02:29:07 +0300

    refactor: format and improve component

--- src/components/Progress.vue
+++ src/components/Progress.vue
@@ -1,15 +1,7 @@
 <template>
   <div class="bg-gray-200 bg-opacity-25 rounded-lg h-2">
     <div
-      class="
-        transition-all
-        bg-gradient-to-r
-        from-red-500
-        via-blue-500
-        to-green-500
-        rounded-lg
-        h-2
-      "
+      class="transition-all opacity-75 bg-gradient-to-r from-red-500 via-blue-500 to-green-500 rounded-lg h-2"
       :style="getStyles"
     ></div>
   </div>
@@ -21,73 +13,73 @@
 </template>
 
 <script lang="ts" setup>
-import { computed, defineProps, onMounted, ref } from "vue";
+import { computed, defineProps, onMounted, ref } from "vue"
 
 const props = defineProps({
   start: {
     type: Number,
     required: true,
-    default: 0,
+    default: 0
   },
   end: {
     type: Number,
     required: true,
-    default: 0,
-  },
-});
+    default: 0
+  }
+})
 
 // Use a reference to generate reactive time.
-const time = ref(new Date().getTime());
+const time = ref(new Date().getTime())
 
 // Updated reactive time every 100 milliseconds.
 onMounted(() => {
   setInterval(() => {
-    time.value = new Date().getTime();
-  }, 100);
-});
+    time.value = new Date().getTime()
+  }, 100)
+})
 
 /**
  * Returns elapsed and left time information.
  */
 const getTime = computed(() => {
   if (props.start === 0 && props.end === 0)
-    return { elapsed: "00:00", left: "00:00" };
+    return { elapsed: "00:00", left: "00:00" }
 
-  const timeElapsed = time.value - props.start;
-  const timeLeft = props.end - time.value;
+  const timeElapsed = time.value - props.start
+  const timeLeft = props.end - time.value
 
   const timeElapsedArray = [
-    Math.round((timeElapsed / (1000 * 60)) % 60),
-    Math.round((timeElapsed / 1000) % 60),
-  ];
+    Math.round((timeElapsed / (1000 * 60)) % 59),
+    Math.round((timeElapsed / 1000) % 59)
+  ]
 
   const timeLeftArray = [
-    Math.round((timeLeft / (1000 * 60)) % 60),
-    Math.round((timeLeft / 1000) % 60),
-  ];
+    Math.round((timeLeft / (1000 * 60)) % 59),
+    Math.round((timeLeft / 1000) % 59)
+  ]
 
-  const mapFunction = (time: number) => `0${time}`.slice(-2);
+  const mapFunction = (time: number) => `0${time}`.slice(-2)
 
   return {
     elapsed: timeElapsedArray.map(mapFunction).join(":"),
-    left: timeLeftArray.map(mapFunction).join(":"),
-  };
-});
+    left: timeLeftArray.map(mapFunction).join(":")
+  }
+})
 
 /**
  * Returns the width percentage of progress bar.
  */
 const getStyles = computed(() => {
-  const total = props.end - props.start;
-  const progress = 100 - (100 * (props.end - time.value)) / total;
+  const total = props.end - props.start
+  const progress = 100 - (100 * (props.end - time.value)) / total
 
   if (progress > 100)
     return {
-      width: "100%",
-    };
+      width: "100%"
+    }
   else
     return {
-      width: `${progress.toFixed(2)}%`,
-    };
-});
+      width: `${progress.toFixed(2)}%`
+    }
+})
 </script>
