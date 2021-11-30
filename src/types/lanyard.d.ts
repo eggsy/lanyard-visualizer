2075dec6d78414b531a0d27e63c8fee8a54915dd
Abdulbaki Dursun <eggsydev@gmail.com>
2021-12-01 00:43:33 +0100

    refactor: update types

--- src/types/lanyard.d.ts
+++ src/types/lanyard.d.ts
@@ -1,76 +1,81 @@
 /* Ripped from my own module, @eggsydev/vue-lanyard */
 export interface LanyardResponse {
-  success: boolean;
-  data: LanyardData;
-  error?: LanyardError;
+  success: boolean
+  data: LanyardData
+  error?: LanyardError
 }
 
 export interface LanyardError {
-  message: string;
-  code: string;
+  message: string
+  code: string
 }
 
 export interface LanyardData {
-  spotify: Spotify | null;
-  discord_user: Discorduser;
-  discord_status: "online" | "dnd" | "offline" | "idle";
-  activities: Activity[];
-  listening_to_spotify: boolean;
-  active_on_discord_mobile: boolean;
-  active_on_discord_desktop: boolean;
+  spotify: Spotify | null
+  discord_user: Discorduser
+  discord_status: "online" | "dnd" | "offline" | "idle"
+  activities: Activity[]
+  listening_to_spotify: boolean
+  active_on_discord_mobile: boolean
+  kv?: Kv
+  active_on_discord_desktop: boolean
+}
+
+export interface Kv {
+  [key: string]: string
 }
 
 export interface Spotify {
-  track_id: string;
-  timestamps: Timestamps;
-  song: string;
-  artist: string;
-  album_art_url: string;
-  album: string;
+  track_id: string
+  timestamps: Timestamps
+  song: string
+  artist: string
+  album_art_url: string
+  album: string
 }
 
 export interface Timestamps {
-  start: number;
-  end?: number;
+  start: number
+  end?: number
 }
 
 export interface Activity {
-  type: number;
-  state: string;
-  name: string;
-  id: string;
-  emoji?: Emoji;
-  created_at: number;
-  application_id?: string;
-  timestamps?: Timestamps;
-  session_id?: string;
-  details?: string;
-  buttons?: string[];
-  assets?: Assets;
+  type: number
+  state: string
+  name: string
+  id: string
+  emoji?: Emoji
+  created_at: number
+  application_id?: string
+  timestamps?: Timestamps
+  session_id?: string
+  details?: string
+  buttons?: string[]
+  assets?: Assets
 }
 
 export interface Assets {
-  small_text: string;
-  small_image: string;
-  large_text: string;
-  large_image: string;
+  small_text: string
+  small_image: string
+  large_text: string
+  large_image: string
 }
 
 export interface Timestamps {
-  start: number;
-  end?: number;
+  start: number
+  end?: number
 }
 
 export interface Emoji {
-  name: string;
-  id: string;
-  animated: boolean;
+  name: string
+  id: string
+  animated: boolean
 }
 
 export interface Discorduser {
-  username: string;
-  public_flags: number;
-  id: string;
-  discriminator: string;
-  avatar: string | null;
+  username: string
+  public_flags: number
+  id: string
+  discriminator: string
+  avatar: string | null
 }
