1375eb0a11d3e63f6287e9107cd8dcc17ad62099
Abdulbaki Dursun <eggsydev@gmail.com>
2021-12-01 01:46:51 +0100

    refactor: move backgrounds to separate file

---
+++ src/data/backgrounds.ts
@@ -0,0 +1,7 @@
+export default [
+  "from-blue-400 via-blue-500 to-blue-600",
+  "from-indigo-400 via-indigo-500 to-indigo-600",
+  "from-red-300 via-red-400 to-red-500",
+  "bg-black",
+  "bg-gray-700",
+]
