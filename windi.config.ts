10fa810a1a75b9218e13bb1643be71b2c7b7d242
Abdulbaki Dursun <eggsydev@gmail.com>
2021-12-01 01:47:01 +0100

    refactor: add backgrounds to safelist

--- windi.config.ts
+++ windi.config.ts
@@ -4,19 +4,22 @@ import { resolve } from "path"
 import LineClamp from "windicss/plugin/line-clamp"
 import defaultTheme from "windicss/defaultTheme"
 
+// Import backgrounds
+import backgrounds from "./src/data/backgrounds"
+
 export default defineConfig({
+  safelist: [...backgrounds],
   theme: {
     fontFamily: {
-      sans: ["Inter", ...defaultTheme.fontFamily.sans]
-    }
+      sans: ["Inter", ...defaultTheme.fontFamily.sans],
+    },
   },
   extract: {
     include: ["index.html", resolve("./src/**/*.{vue,html,jsx,tsx}")],
-    exclude: ["node_modules", ".git"]
+    exclude: ["node_modules", ".git"],
   },
   shortcuts: {
-    btn:
-      "bg-gray-100 bg-opacity-10 hover:bg-opacity-25 transition px-4 py-2 rounded-lg text-white"
+    btn: "bg-gray-100 bg-opacity-10 hover:bg-opacity-25 transition px-4 py-2 rounded-lg text-white",
   },
-  plugins: [LineClamp]
+  plugins: [LineClamp],
 })
