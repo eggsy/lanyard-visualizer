0a58c006af13c78a1576dac57be02e2ad885e03e
Abdulbaki Dursun <eggsydev@gmail.com>
2023-01-27 00:14:13 +0300

    chore: remove all options

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,10 @@
-<script lang="ts" setup>
-import { computed, ref } from "vue"
-import { useRoute } from "vue-router"
-
-// Import compnoents
-import Options from "./components/Options.vue"
-
-// Import backgrounds
-import backgrounds from "./data/backgrounds"
-
-// Component options
-const route = useRoute()
-const getOptions = computed(() => {
-  const { name, query } = route
-
-  return {
-    name,
-    query,
-    mode: query.mode,
-    background: query.background !== "false",
-  }
-})
-
-const background = ref(
-  backgrounds[Math.floor(Math.random() * backgrounds.length)]
-)
-
-const toggleTheme = (theme: string) => {
-  background.value = theme
-}
-</script>
-
 <template>
-  <Options :currentBg="background" @updateTheme="toggleTheme" />
-
-  <div
-    class="bg-gradient-to-tl flex min-h-screen text-white items-center justify-center"
-    :class="{
-      'bg-black': getOptions.background === false,
-      [background]: getOptions.background === true,
-
-      'px-4': getOptions.mode !== 'iframe',
-    }"
-  >
-    <div
-      :class="
-        getOptions.mode !== 'iframe' ? 'container mx-auto' : 'w-screen h-screen'
-      "
-    >
-      <router-view v-slot="{ Component }">
-        <transition name="fade" mode="out-in">
-          <component :is="Component" />
-        </transition>
-      </router-view>
-    </div>
+  <div class="min-h-screen text-white">
+    <router-view v-slot="{ Component }">
+      <transition name="fade" mode="out-in">
+        <component :is="Component" />
+      </transition>
+    </router-view>
   </div>
 </template>
 
@@ -64,6 +16,10 @@ const toggleTheme = (theme: string) => {
 html {
   scrollbar-width: thin;
   scrollbar-width: 6px;
+}
+
+body {
+  @apply bg-dark-900;
 }
 
 html::-webkit-scrollbar {
