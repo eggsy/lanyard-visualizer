b71b267af8ad6669fa16d0158acc74ccff8f3afc
Abdulbaki Dursun <eggsydev@gmail.com>
2023-01-27 00:13:52 +0300

    refactor: update routes object

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,6 @@
 // Import routes
 import HomeComponent from "./views/index.vue"
-import UserComponent from "./views/user/[id].vue"
+import UserComponent from "./views/[id].vue"
 
 // Import types
 import { RouteRecordRaw } from "vue-router"
@@ -9,19 +9,19 @@ const RouterConfig: RouteRecordRaw[] = [
   {
     path: "/",
     name: "Home",
-    component: HomeComponent
+    component: HomeComponent,
   },
   {
-    path: "/user/:id",
-    alias: "/users/:id",
+    path: "/:id",
+    alias: ["/user/:id", "/users/:id", "/u/:id"],
     name: "User",
     component: UserComponent,
-    props: true
+    props: true,
   },
   {
     path: "/:pathMatch(.*)*",
-    redirect: "/"
-  }
+    redirect: "/",
+  },
 ]
 
 // Export router config
