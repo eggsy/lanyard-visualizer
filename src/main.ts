327b2153fd248d94f51349ca86aa6d3f32de043f
Abdulbaki Dursun <eggsydev@gmail.com>
2023-01-27 00:31:29 +0300

    chore: use motion plugin

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,15 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import routes from "./routes"
+import App from "./App.vue"
 
-// Import Vue router, routes and create the router
-import { createRouter, createWebHistory } from "vue-router";
-import routes from "./routes";
+import { createApp } from "vue"
+import { MotionPlugin } from "@vueuse/motion"
+import { createRouter, createWebHistory } from "vue-router"
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-});
+})
 
-// Import Windi CSS
-import "virtual:windi.css";
+import "virtual:windi.css"
 
-// Create and mount the app
-createApp(App)
-  .use(router)
-  .mount("#app");
+createApp(App).use(router).use(MotionPlugin).mount("#app")
