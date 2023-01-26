11321f5d1908b2ccc73c03e50e27a777e5b0aff5
Abdulbaki Dursun <eggsydev@gmail.com>
2023-01-27 00:18:44 +0300

    chore: update shortcut to support icons

--- windi.config.ts
+++ windi.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
     exclude: ["node_modules", ".git"],
   },
   shortcuts: {
-    btn: "bg-white/5 hover:bg-white/10 text-white/30 hover:text-white/50 select-none transition px-4 py-2 text-sm rounded-lg",
+    btn: "bg-white/5 hover:bg-white/10 flex space-x-2 items-center text-white/30 hover:text-white/50 select-none transition px-4 py-2 text-sm rounded-lg",
   },
   plugins: [LineClamp],
 })
